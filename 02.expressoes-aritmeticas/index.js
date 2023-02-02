a9a13864e0357f3bacd49b519868e0b219cd9f72
Gustavo Borges <80328652+distnrb@users.noreply.github.com>
2023-02-02 12:26:39 -0300

    Add files via upload

---
+++ 02.expressoes-aritmeticas/index.js
@@ -0,0 +1,30 @@
+/* EXPRESSÕES ARITMÉTICAS
+    expressões aritméticas é uma combinação de...
+    operandos (valores, variaveis, etc...)
+    operadores ( + - * / %)
+    ex: variavel = variavel1 - 5;
+*/
+
+let alunos = 15;
+
+//alunos = alunos + 1;
+//alunos = alunos - 1;
+//alunos = alunos * 2;
+//alunos = alunos / 2;
+//alunos = alunos % 1,5;
+
+//alunos+=1;
+//alunos-=1;
+//alunos*=2;
+//alunos/=2;
+
+/*
+    ordem de operadores
+    1. parênteses ()
+    2. potência, expoentes
+    3. multiplicação e divisão
+    4. adição e subtração
+*/
+
+let resultado = 10 + 5 * (5 - 2);
+console.log(resultado);
